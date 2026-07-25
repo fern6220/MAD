d00ceb2a49c9e0cb0b4294cf098ead42188251f7
Obet Fernando <obetfernando6220@gmail.com>
2026-07-25 11:34:41 +0800

    Done with Lab #5: React Native – Basic Components

---
+++ L05_react_native_basic_components/BasicComponentsDemo.js
@@ -0,0 +1,94 @@
+import { Text, View, StyleSheet, Pressable, Image } from 'react-native';
+import {useState} from 'react';
+  
+export default function BasicComponentsDemo() {
+  const [message, setMessage] = useState('Welcome');
+
+  function displayMessage() {
+    setMessage("Hello! Welcome to my React native applicatiion");
+  }
+  return(
+      <View style={styles.container}>
+
+          <>
+            <Image
+              source={{uri: 'https://avatars.githubusercontent.com/u/263922665?v=4'}}
+              style={styles.profileImage}
+            />
+          </>
+
+        <Text style={styles.name}> Obet Fernando </Text>
+        
+        <Text style={styles.course}> Associate in Computer Technology 2 </Text>
+        
+        <Text style={styles.description}> ACT Student | Exploring Web Development, Programming, and Information Systems Solutions. Minecraft.</Text>
+        
+        
+        <Text style={styles.message}>{message}</Text>
+
+      <Pressable onPress={displayMessage}>
+        <Text style={styles.button}>Say Hello</Text>
+      </Pressable>
+      </View>
+     
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+    backgroundColor: "#f2f4f7",
+  },
+
+  profileImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 50,
+  },
+
+  name: {
+    marginBottom: 8,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  course: {
+    marginBottom: 16,
+    fontSize: 17,
+    textAlign: "center",
+  },
+
+  description: {
+    marginBottom: 20,
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: "center",
+  },
+
+  message: {
+    marginBottom: 20,
+    fontSize: 17,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+
+  button: {
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    backgroundColor: "#1d4ed8",
+    borderRadius: 8,
+  },
+
+  buttonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+
